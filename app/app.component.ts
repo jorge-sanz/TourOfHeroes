ebe92be9c2ebb9496aab2b84c9116bf282013cf6
Jorge Sanz Pérez <jorsanzp@gmail.com>
2016-06-27 13:21:25 +0200

    Add two-way binding to the hero editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,11 +9,15 @@ export class Hero {
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>`
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `
 })
 export class AppComponent { 
   title = 'Tour of Heroes';
-  hero: Hero {
+  hero: Hero = {
     id: 1,
     name: 'Windstorm' 
   };
