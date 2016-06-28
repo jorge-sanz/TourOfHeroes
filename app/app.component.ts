005cb23174ce368676fe11ac295b589cf08db687
Jorge Sanz Pérez <jorsanzp@gmail.com>
2016-06-28 13:40:42 +0200

    Add selecting hero feature

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,15 +9,17 @@ export class Hero {
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
     </div>
   `,
   styles: [`
@@ -72,11 +74,9 @@ export class Hero {
 })
 export class AppComponent { 
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm' 
-  };
+  selectedHero: Hero;
   public heroes = HEROES;
+  onSelect(hero: Hero) { this.selectedHero = hero; };
 }
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -90,6 +90,8 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
+
+
 
 
 
