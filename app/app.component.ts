bab9e9508f8c563244d681f12a7a79926ae84780
Jorge Sanz Pérez <jorsanzp@gmail.com>
2016-07-02 16:55:47 +0200

    Add heroes routing before adding dashboard

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,19 @@
-import { Component }       from '@angular/core';
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 import { HeroService }     from './hero.service';
-import { HeroesComponent } from './heroes.component';
 @Component({
-    selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <my-heroes></my-heroes>
-    `,
-    directives: [HeroesComponent],
-    providers: [
-        HeroService
-    ]
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <a [routerLink]="['/heroes']">Heroes</a>
+    <router-outlet></router-outlet>
+  `,
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    HeroService
+  ]
 })
 export class AppComponent {
-    title = 'Tour of Heroes';
+  title = 'Tour of Heroes';
 }
 
