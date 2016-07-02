d2ca38be96012748f992bfff73b4d1b7f2f61cb2
Jorge Sanz Pérez <jorsanzp@gmail.com>
2016-07-02 17:25:18 +0200

    'Add a dashboard' step

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { HeroService }     from './hero.service';
 @Component({
-  selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <a [routerLink]="['/heroes']">Heroes</a>
-    <router-outlet></router-outlet>
-  `,
-  directives: [ROUTER_DIRECTIVES],
-  providers: [
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <nav>
+            <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
+            <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
     HeroService
-  ]
+    ]
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+    title = 'Tour of Heroes';
 }
 
