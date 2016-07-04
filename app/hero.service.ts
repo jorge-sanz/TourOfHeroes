ae4cac85e961c96423d0d7aa8172686a39ae7ee2
Jorge Sanz Pérez <jorsanzp@gmail.com>
2016-07-04 12:37:00 +0200

    'Heroes and HTTP' step

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import 'rxjs/add/operator/toPromise';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { XHRBackend } from '@angular/http';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
+
     private heroesUrl = 'app/heroes'; // URL to web api
 
     constructor(private http: Http) { }
@@ -17,6 +17,51 @@ export class HeroService {
                    .toPromise()
                    .then(response => response.json().data)
                    .catch(this.handleError);
+    }
+
+    // Add new Hero
+    private post(hero: Hero): Promise<Hero> {
+        let headers = new Headers({
+        'Content-Type': 'application/json'});
+
+        return this.http
+                   .post(this.heroesUrl, JSON.stringify(hero), {headers: headers})
+                   .toPromise()
+                   .then(res => res.json().data)
+                   .catch(this.handleError);
+    }
+
+    // Update existing Hero
+    private put(hero: Hero) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        let url = `${this.heroesUrl}/${hero.id}`;
+
+        return this.http
+                   .put(url, JSON.stringify(hero), {headers: headers})
+                   .toPromise()
+                   .then(() => hero)
+                   .catch(this.handleError);
+    }
+
+    delete(hero: Hero) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        let url = `${this.heroesUrl}/${hero.id}`;
+
+        return this.http
+                   .delete(url, headers)
+                   .toPromise()
+                   .catch(this.handleError);
+    }  
+
+    save(hero: Hero): Promise<Hero>  {
+        if (hero.id) {
+            return this.put(hero);
+        }
+        return this.post(hero);
     }
 
     private handleError(error: any) {
